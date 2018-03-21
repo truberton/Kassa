c5ebe92d1144eb16d63deba9d4c28088389a1f6d
Truberton <gertandry@gmail.com>
2018-03-21 16:24:35 +0200

    Tehtud

---
+++ WpfList/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kassa
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Ostukorv> list { get; private set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            list = new List<Ostukorv>();
+        }
+
+        private void todoListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnLisa_Click(object sender, RoutedEventArgs e)
+        {
+            if (Convert.ToDouble(Hind.Text) > 0)
+            {
+                Ostukorv_Listview.Items.Add(new Ostukorv { Nimi = Nimetus.Text, Kogus = int.Parse(Kogus.Text), Hind = Convert.ToDouble(Hind.Text) });
+                list.Add(new Ostukorv { Nimi = Nimetus.Text, Kogus = int.Parse(Kogus.Text), Hind = Convert.ToDouble(Hind.Text) });
+            }
+        }
+
+        private void Eemalda_Click(object sender, RoutedEventArgs e)
+        {
+            Ostukorv_Listview.Items.Remove(Ostukorv_Listview.SelectedItem);
+        }
+
+        private void Osta_Click(object sender, RoutedEventArgs e)
+        {
+            var tšekk = new Tšekk();
+            tšekk.Print(list);
+        }
+    }
+
+    public class Ostukorv
+    {
+        public string Nimi { get; set; }
+        public int Kogus { get; set; }
+        public double Hind { get; set; }
+    }
+}
---
+++ WpfList/Tšekk.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace Kassa
+{
+    class Tšekk
+    {
+        public void Print(List<Ostukorv> list)
+        {
+            List<string> Tekst = new List<string>();
+            double kokku = 0;
+            foreach (var item in list)
+            {
+                Tekst.Add(string.Format("{0}x {1} - €{2}", Convert.ToString(item.Kogus), item.Nimi, item.Hind));
+                kokku += item.Hind * item.Kogus;
+            }
+            File.WriteAllLines("Tšekk.txt", Tekst);
+            File.AppendAllText("Tšekk.txt", "-------------------------" + Environment.NewLine + "Kokku: €" + kokku);
+
+            Process.Start("Tšekk.txt");
+        }
+    }
+}
